cd38365e489998bfa77cc0a45a3a0a6fa45e09b4
asly.cantillo <asly.cantillo@ludycom.com>
2023-04-25 18:23:09 -0500

    first commit

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
 
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs')
-                .option('b', {
-                    alias: 'base',
-                    type: 'number',
-                    demandOption: true
-                })
-                .option('l', {
-                    alias: 'listar',
-                    type: 'boolean',
-                    demandOption: true,
-                    default: false
-                })
-
-                .check((argv, options)=> {
-                    if(isNaN(argv.b)){
-                        throw 'mi amigo Efrain y mi amigo Esteban <(°-°)> (=^.^=)'
-                    }
-                    return true;
-                })
-                .argv;
-
-                
+require('colors');
+const argv = require('./config/yargs');
+           
 console.clear();
-console.log(argv);
 console.log('base: yargs', argv.b);
 
 crearArchivo(argv.b, argv.l)
-  .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+  .then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
   .catch(err => console.log(err));
 
 
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const colors = require('colors');
 const crearArchivo = async (base = 8, listar  = false) => {
 
     try {
@@ -6,13 +7,13 @@ const crearArchivo = async (base = 8, listar  = false) => {
     
         for (let i = 1; i <= 10; i++) {
     
-            salida += (`${base}x ${i}= ${base * i}\n`);
+            salida += (`${base} ${'x'.magenta} ${i} ${'='.yellow} ${base * i}\n`);
         }
     
         if ( listar ){
-            console.log('=================')
-            console.log('Tabla del:' , base)
-            console.log('=================');
+            console.log('================='.green);
+            console.log('Tabla del:' , colors.blue(base));
+            console.log('================='.green);
             console.log(salida);
             }
        
