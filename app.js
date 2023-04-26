c9e26db7d7b678731700477587c039f89403990b
asly.cantillo <asly.cantillo@ludycom.com>
2023-04-26 10:12:05 -0500

    terminado

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-
 const { crearArchivo } = require('./helpers/multiplicar');
-require('colors');
 const argv = require('./config/yargs');
-           
+
+require('colors');
+
+
 console.clear();
-console.log('base: yargs', argv.b);
 
-crearArchivo(argv.b, argv.l)
-  .then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
-  .catch(err => console.log(err));
-
-
-
-
-
-
-
-
-
-
-
-
-// const [, , arg3 = 'base=5']= process.argv;
-// const [, base = 5]= arg3.split('=');
-// console.log(base);
-
-// const base= 6;
-
-
-// crearArchivo(base)
-// .then(nombreArchivo  => console.log(nombreArchivo, 'creado'))
-// .catch(err => console.log(err));
-
-
-
+crearArchivo( argv.b, argv.l, argv.h )
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado') )
+    .catch( err => console.log(err) );
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,33 +1,38 @@
 const fs = require('fs');
 const colors = require('colors');
-const crearArchivo = async (base = 8, listar  = false) => {
+
+const crearArchivo = async( base = 5, listar = false, hasta = 10 ) => {
 
     try {
-        let salida = '';
-    
-        for (let i = 1; i <= 10; i++) {
-    
-            salida += (`${base} ${'x'.magenta} ${i} ${'='.yellow} ${base * i}\n`);
+
+        let salida  = '';
+        let consola = '';
+        
+        for( let i = 1; i <= hasta; i++ ) {
+            salida  += `${ base } x ${ i } = ${ base * i }\n`;
+            consola += `${ base } ${ 'x'.green } ${ i } ${ '='.green } ${ base * i }\n`;
         }
-    
-        if ( listar ){
-            console.log('================='.green);
-            console.log('Tabla del:' , colors.blue(base));
-            console.log('================='.green);
-            console.log(salida);
-            }
-       
-    
-        fs.writeFileSync(`tabla-${base}.txt`, salida,);
-    
-        return(`tabla-${base}.txt`);
+
+        if ( listar ) {
+            console.log('===================='.green);
+            console.log('   Tabla del:'.green, colors.blue( base ) );
+            console.log('===================='.green);
+
+            console.log(consola);
+        }
+        
+
+        fs.writeFileSync( `./salida/tabla-${ base }.txt`, salida );
+
+
+        return `tabla-${ base }.txt`;
         
     } catch (err) {
         throw err;
     }
-    
+
 }
 
-module.exports= {
+module.exports = {
     crearArchivo
 }
